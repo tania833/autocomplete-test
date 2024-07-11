63f85c46e3713168eeedec1f11e85acf27382ad3
Tetiana Pastukh <tetiana.polishchuk@digitalturbine.com>
2024-07-11 17:10:20 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ import './App.css';
 const queryClient = new QueryClient()
 
 function App() {
-  // const bears = useStore((state) => state.bears)
-  // const increaseBears = useStore(state => state.increasePopulation)
-
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
